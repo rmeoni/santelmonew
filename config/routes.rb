8a479a987caeb1e9297c924e80f056965c03f9ff
rmeoni <rodrigo.me13@gmail.com>
2019-08-22 16:30:50 -0600

    adding images and styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 get '/' => 'pages#home', as: :root
+get '/gallery' => 'pages#pictures'
+get '/menu' => 'pages#menu'
+
+
 get "*missing" => redirect("/")
+
 end
